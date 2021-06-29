5d50a3d3acdb032f287f7aa7eea1fef76086f82d
Angelo A. Bezutti <65688127+AngeloBezutti@users.noreply.github.com>
2021-06-29 20:10:42 -0300

    "Start Project"

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />git remote add origin https://github.com/AngeloBezutti/rick-and-morty.git
+git branch -M main
+git push -u origin main
+  </React.StrictMode>,
+  document.getElementById('root')
+);
